9560329ffeb65b8475c975316d9edc6c8f5940c3
NauvalNabil <nauvalnabil100@gmail.com>
2025-07-16 23:08:36 +0700

    Membuat Laravel

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,5 @@
+<svg class="text-pink-500" id="logo-38" width="150" height="" viewBox="0 0 78 32" fill="#ffc0cb" xmlns="http://www.w3.org/2000/svg">
+    <path d="M55.5 0H77.5L58.5 32H36.5L55.5 0Z" class="ccustom" fill="#ff69b4"></path>
+    <path d="M35.5 0H51.5L32.5 32H16.5L35.5 0Z" class="ccompli1" fill="#ffb6d5"></path>
+    <path d="M19.5 0H31.5L12.5 32H0.5L19.5 0Z" class="ccompli2" fill="#ffc0cb"></path>
+</svg>
